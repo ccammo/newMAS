e68143efe4724c3f6f800f1c11438f080c490b38
Christopher Camillucci <christopher.camillucci@gmail.com>
2018-01-05 17:36:56 -0500

    first commit removing curley brackets

--- public/create.php
+++ public/create.php
@@ -7,7 +7,7 @@
  */
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:masserver.database.windows.net,1433; Database = masDB", "yokota", "{Sports12!@}");
+    $conn = new PDO("sqlsrv:server = tcp:masserver.database.windows.net,1433; Database = masDB", "yokota", "Sports12!@");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -16,7 +16,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "yokota@masserver", "pwd" => "{Sports12!@}", "Database" => "masDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "yokota@masserver", "pwd" => "Sports12!@", "Database" => "masDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:masserver.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
@@ -29,7 +29,7 @@ if (isset($_POST['submit']))
 
 	try 
 	{
-		$connection = new PDO("sqlsrv:server = tcp:masserver.database.windows.net,1433; Database = masDB", "yokota", "{Sports12!@}");
+		$connection = new PDO("sqlsrv:server = tcp:masserver.database.windows.net,1433; Database = masDB", "yokota", "Sports12!@");
 		
 		$new_user = array(
 			"firstname" => $_POST['firstname'],
