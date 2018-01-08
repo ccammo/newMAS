82865d1870e2b3313149adcdd51de540ca8026e9
Christopher Camillucci <christopher.camillucci@gmail.com>
2018-01-08 13:51:38 -0500

    Updated config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
  */
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:masserver.database.windows.net,1433; Database = masDB", "yokota", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:masserver.database.windows.net,1433; Database = masDB", "yokota", "Sports12!@");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -15,7 +15,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "yokota@masserver", "pwd" => "{your_password_here}", "Database" => "masDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "yokota@masserver", "pwd" => "Sports12!@", "Database" => "masDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:masserver.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
@@ -23,8 +23,8 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 $host       = "tcp:masserver.database.windows.net,1433";
 $username   = "yokota";
-$password   = "Password1!";
-$dbname     = "massserver"; //will use later
+$password   = "Sports12!@!";
+$dbname     = "masDB"; //will use later
 $dsn        = '';//"mysql:host=$host;dbname=$dbname"; //will use later
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
