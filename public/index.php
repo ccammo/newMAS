0a5589eb42ac1f761e5acc3ddf77e1fa09081a67
Christopher Camillucci <christopher.camillucci@gmail.com>
2018-01-18 12:59:22 -0500

    Got rid of crazy HEAD markups

--- public/index.php
+++ public/index.php
@@ -5,33 +5,31 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<<<<<<< HEAD
+
 
 	<title>Simple Database App</title>
 
-=======
+
 <div class="title">
 	<title>Simple Database App</title>
 </div>
->>>>>>> 6f75ed9462092ec30a0e1940292a484baeb4a49e
+
 	<link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-<<<<<<< HEAD
-=======
+
 	<div class="wrapper">
->>>>>>> 6f75ed9462092ec30a0e1940292a484baeb4a49e
+
 	<h1>Simple Database App</h1>
 
 	<ul>
 		<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
 		<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
 	</ul>
-<<<<<<< HEAD
-=======
+
 </div>
->>>>>>> 6f75ed9462092ec30a0e1940292a484baeb4a49e
+
 
 </body>
 </html>
